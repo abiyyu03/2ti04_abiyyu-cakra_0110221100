737da0a09ff0dc167584693bdd8b5e376df388ee
abiyyu03 <abiyyu652@gmail.com>
2022-03-05 10:51:52 +0700

    move bootstrap to root folder

--- praktikum01/array_siswa.php
+++ praktikum01/array_siswa.php
@@ -37,7 +37,7 @@ $ar_nilai = [$ns1,$ns2,$ns3,$ns4];
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
-	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
 </head>
 <body>
 	<div class="container">
