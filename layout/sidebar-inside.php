758aec0ee730c097965a7bddb283c6035cfc61e1
abiyyu03 <abiyyu652@gmail.com>
2022-04-14 11:20:55 +0700

    create layout

---
+++ layout/sidebar-inside.php
@@ -0,0 +1,175 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="/2ti04_Abiyyu-cakra_0110221100" class="brand-link">
+      <img src="../assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">AdminLTE 3</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="../assets/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Abiyyu Cakra</a>
+        </div>
+      </div>
+
+      <!-- SidebarSearch Form -->
+      <div class="form-inline">
+        <div class="input-group" data-widget="sidebar-search">
+          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+          <div class="input-group-append">
+            <button class="btn btn-sidebar">
+              <i class="fas fa-search fa-fw"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-edit"></i>
+              <p>
+                Praktikum01
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="../praktikum01/array_siswa.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Array Siswa</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-edit"></i>
+              <p>
+                Praktikum02
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="../praktikum02/form_nilai.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Form Nilai</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="../praktikum02/form_belanja.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Form Belanja</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-edit"></i>
+              <p>
+                Praktikum03
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="../praktikum03/nilai_siswa.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Nilai Siswa</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-edit"></i>
+              <p>
+                Praktikum04
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="../praktikum04/daftar_mahasiswa.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Daftar Mahasiswa</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="../praktikum04/data_lingkaran.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Data Lingkaran</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="../praktikum04/data_persegi_panjang.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Data Persegi Panjang</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-edit"></i>
+              <p>
+                Praktikum05
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="../praktikum05/account_bank.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Account Bank</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="../praktikum05/data_dispenser.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Data Dispenser</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-edit"></i>
+              <p>
+                Praktikum06
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="../praktikum06/index.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>BMI Pasien</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          
+          <li class="nav-item">
+            <a href="https://github.com/abiyyu03/2ti04_Abiyyu-cakra_0110221100" target="_blank" class="nav-link">
+              <i class="fa fa-github-square nav-icon"></i>
+              <p>Get Source code</p>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
